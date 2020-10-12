40f2cf5b73229aaa5bef93599c0242b06d37e8b1
Gonzalo Salazar <gonzalo.sc2@gmail.com>
2020-10-12 18:20:03 -0400

    proj2: def of some functions

--- Blackjack/bj_modules.py
+++ Blackjack/bj_modules.py
@@ -21,6 +21,7 @@ ranks = ('Two','Three','Four','Five','Six','Seven','Eight','Nine','Ten', \
          'Jack','Queen','King','Ace')
 values = {'Two':2,'Three':3,'Four':4,'Five':5,'Six':6,'Seven':7,'Eight':8, \
           'Nine':9,'Ten':10,'Jack':10,'Queen':10,'King':10,'Ace':11}
+playing = True
 
 ### CLASSESS ###
 class Card:
@@ -52,6 +53,14 @@ class Deck:
             deck_in_string += '\n' + card.__str__()
 
         return 'Actual Deck: \n' + deck_in_string
+
+    def __len__(self):
+        count = 0
+
+        for card in self.deck:
+            count += 1
+
+        return count
 
     def shuffle(self):
         random.shuffle(self.deck)
@@ -103,4 +112,94 @@ class Chips:
     def lose_bet(self):
         self.total -= self.bet
 
+class BalanceError(Exception):
+    "Used when bet amount is larger than the available balance"
+    pass
+
 ### FUNCTIONS ###
+def take_bet(chips):
+    "Asks the user for an integer value and checks that a Player's bet can be"
+    "covered by their available chips."
+
+    while True:
+        try:
+            chips.bet = int(input('How much do you want to bet?: '))
+
+            if chips.total < chips.bet:
+                raise BalanceError('Bet amount is larger than the available balance.')
+
+            return bet
+
+        except ValueError:
+            print('The value entered is wrong. Please use interger values.')
+
+        except BalanceError:
+            print('Bet amount is larger than the available balance.' + \
+                  '\nPlease bet an amount less than or equal to your available balance: ' + \
+                  str(chips.total))
+
+def hit(deck,hand):
+    "Deals one card off the deck and add it to the Hand. As well as checks for"
+    "aces in the event that a player's hand exceeds 21."
+
+    deal = deck.deal()
+    hand.add_card(deal)
+    hand.adjust_for_ace()
+
+def hit_or_stand(deck,hand):
+    "Controls the behavior of the while loop associated with the player's turn."
+
+    global playing  # to control an upcoming while loop
+    hit_values = ('H','HIT')
+    stand_values = ('S','STAND')
+
+    while True:
+        try:
+            answer = input('Do you want to HIT(H) or STAND(S)?: ')
+            if not answer.upper() in hit_values and not answer.upper() in stand_values:
+                raise 'Invalid input.'
+
+            if answer.upper() == 'H' or 'HIT':
+                hit(deck,hand)
+            else:
+                print("Player stands. Dealer's turn begins.")
+                playing = False
+
+            break
+        except:
+            print('Invalid answer. Please answer HIT or STAND.')
+
+def show_some(player,dealer):
+    "Show the cards on the board, hidding the first card on the dealer's hand"
+
+    print("Dealer's hand:")
+    print("\n<Covered Card>", str(dealer.cards[1]), sep = ' / ')
+    print('\n')
+
+    print("Player's hand:")
+    print(*player.cards, sep = ' / ')
+
+def show_all(player,dealer):
+    "Show all the cards on the board"
+
+    print("Dealer's hand:")
+    print(*dealer.cards, sep = ' / ')
+    print('\n')
+
+    print("Player's hand:")
+    print(*player.cards, sep = ' / ')
+
+    
+
+
+
+
+
+# deck = Deck()
+# dealer = Hand()
+# player = Hand()
+#
+# dealer.add_card(deck.deal())
+# player.add_card(deck.deal())
+#
+# show_all(player,dealer)
